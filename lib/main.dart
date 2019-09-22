20c2c1611a11df715c2303f7a913493329e4a461
Peter <pedromolesp@gmail.com>
2019-09-22 22:20:22 +0200

    #2 Completa a nivel de diseño a falta de añadir grid y visualizar elementos.

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primaryColor: Colors.white,
       ),
-     home: EDteamHome(),
+      home: EDteamHome(),
     );
   }
 }
+
 class EDteamHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,35 +25,122 @@ class EDteamHome extends StatelessWidget {
         title: Image.asset("assets/images/logo.png", height: 40.0),
         actions: <Widget>[
           Padding(
-            padding: const EdgeInsets.fromLTRB(0,0,25,0),
-
-            child: InkWell(child: Image.asset("assets/images/schedule.jpg", height: 20.0, width: 20,),
-              onTap: (){print("Hola munddo");} ,
+            padding: const EdgeInsets.fromLTRB(0, 0, 25, 0),
+            child: InkWell(
+              child: Image.asset(
+                "assets/images/schedule.jpg",
+                height: 20.0,
+                width: 20,
+              ),
+              onTap: () {
+                print("Hola munddo");
+              },
             ),
-
           ),
           Padding(
-            padding: const EdgeInsets.fromLTRB(0,0,25,0),
-
-            child: InkWell(child: Image.asset("assets/images/talk.png", height: 20.0, width: 20,) ,
-              onTap: (){print("Hola munddo");} ,
+            padding: const EdgeInsets.fromLTRB(0, 0, 25, 0),
+            child: InkWell(
+              child: Image.asset(
+                "assets/images/talk.png",
+                height: 20.0,
+                width: 20,
+              ),
+              onTap: () {
+                print("Hola munddo");
+              },
             ),
-
           ),
-
           Icon(Icons.search),
           InkWell(
-            onTap: (){print("Hello bro!");},
+            onTap: () {},
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(40,0,15,0),
-
+              padding: const EdgeInsets.fromLTRB(40, 0, 15, 0),
               child: Icon(Icons.dehaze),
             ),
-
-    )
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              "Continúa estudiando",
+              style: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.w600,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, position) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200.0,
+                    height: 150.0,
+                    color: Colors.blue,
+                    child: Text("Position $position"),
+                  ),
+                );
+              },
+            ),
+          ),
+          DefaultTabController(
+            length: 3,
+            child: Expanded(
+              child: Column(
+                children: <Widget>[
+                  TabBar(
+                    unselectedLabelColor: Colors.grey,
+                    indicatorColor: Colors.lightBlue,
+                    tabs: [
+                      Tab(
+                        text: "Cursos",
+                      ),
+                      Tab(
+                        text: "WorkShops",
+                      ),
+                      Tab(
+                        text: "Especialidades",
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                      children: [
+                        GridView.builder(
+                            gridDelegate: null,
+                            itemBuilder: (context, position){
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  width: 200.0,
+                                  height: 150.0,
+                                  color: Colors.blue,
+                                  child: Text("Position $position"),
+                                ),
+                              );
+                            },
+                        ),
+                        Container(color: Colors.red),
+                        Container(color: Colors.yellow),
+                        Container(color: Colors.blue),
+                      ],
+                    ),
+                  ),
+                 ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
-
 }
